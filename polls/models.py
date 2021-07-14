d5c38b94dde7ef1f39fa9f10accac05bb1f37cca
SophearithRin <rinsophearith@gmail.com>
2021-07-14 15:40:18 +0700

    test commit 1

--- polls/models.py
+++ polls/models.py
@@ -6,17 +6,6 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-sdafsdfsdf
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-
-class Fruit(models.Model):
-    fruit_name = models.ForeignKey(Question, on_delete=models.CASCADE)
-    sweetness_level = models.CharField(max_length=200)
-
 
 class Car(models.Model):
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
